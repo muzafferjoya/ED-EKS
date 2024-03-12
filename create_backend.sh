91884906b3a172ae4630ad3bd0446a211d3e28c3
Muzaffar khan <muzafferjoya@gmail.com>
2024-03-12 11:10:00 +0000

    New

---
+++ create_backend.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -euo pipefail
+
+environment=dev
+AWS_REGION="us-east-1"
+
+# Define names for resources
+BUCKET_NAME="sunbirded-${environment}-tfstate-bucket"
+DYNAMODB_TABLE_NAME="sunbirded-${environment}-tfstate-lock"
+
+# Create S3 bucket
+#aws s3api create-bucket --bucket $BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
+aws s3api create-bucket --bucket $BUCKET_NAME --region $AWS_REGION
+
+# Enable versioning on S3 bucket
+aws s3api put-bucket-versioning --bucket $BUCKET_NAME --versioning-configuration Status=Enabled
+
+# Create DynamoDB table for locking
+aws dynamodb create-table --table-name $DYNAMODB_TABLE_NAME --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --region $AWS_REGION
+
+# Output Terraform backend configuration
+echo "terraform {
+  backend \"s3\" {
+    bucket         = \"$BUCKET_NAME\"
+    key            = \"$environment/terraform.tfstate\"
+    region         = \"$AWS_REGION\"
+    dynamodb_table = \"$DYNAMODB_TABLE_NAME\"
+  }
+}" > backend.tf
+
+echo -e "\nTerraform backend configuration has been created."
